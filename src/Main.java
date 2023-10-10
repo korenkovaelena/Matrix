a4a0630d4c3abd1158196e9182978ba4350f17ba
Aleksandr K <alkaz05@mail.ru>
2023-10-10 15:01:21 +0300

    объявление матрицы

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //хочу чтобы пользователь задал размеры матрицы
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Введите размеры матрицы");
+        int n = scan.nextInt();
+        int m = scan.nextInt();
+        int[][] matr = new int[n][m];
+
     }
 }
