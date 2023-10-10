14cc856812af8b6aa9fc2949a5acc2c3b9b34496
Aleksandr K <alkaz05@mail.ru>
2023-10-10 15:29:01 +0300

    рефакторинг

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,19 @@ import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         //хочу чтобы пользователь задал размеры матрицы
+        int[][] matr = createMatrixOfZeroes();
+        printMatr(matr);
+        randomize(matr);
+        printMatr(matr);
+    }
+
+    private static int[][] createMatrixOfZeroes() {
         Scanner scan = new Scanner(System.in);
         System.out.println("Введите размеры матрицы");
         int n = scan.nextInt();
         int m = scan.nextInt();
         int[][] matr = new int[n][m];
-        printMatr(matr);
-        randomize(matr);
-        printMatr(matr);
+        return matr;
     }
 
     public static void printMatr(int[][] matr)
